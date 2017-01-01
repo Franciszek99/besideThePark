04201e9c1014840884c007bdad1e61c92a5c398c
franciszek99 <franko.gen@gmail.com>
2017-01-01 21:04:49 +0100

    Handelbarsjs done

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
  $(document).ready(function(){
+
+ 	$(function () {
+  // Grab the template script
+  var theTemplateScript = $("#big-address-template").html();
+
+  // Compile the template
+  var theTemplate = Handlebars.compile(theTemplateScript);
+
+  // Define our data object
+  var context={
+    "title": "Beside the park",
+  };
+
+  // Pass our data to the template
+  var theCompiledHtml = theTemplate(context);
+
+  // Add the compiled html to the page
+  $('.big-content-placeholder').html(theCompiledHtml);
+});
+
           $(".fadeOut").addClass("animated infinite flash");
       });
